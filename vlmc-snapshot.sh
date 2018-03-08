70a906f35d07524f9bda908cf8105a43b9c2ec34
Sérgio M. Basto <sergio@serjux.com>
2018-03-08 04:12:21 +0000

    Update vlmc-snapshot.sh

--- vlmc-snapshot.sh
+++ vlmc-snapshot.sh
@@ -12,11 +12,12 @@ cleanup() {
 
 unset CDPATH
 pwd=$(pwd)
-date=$(date +%Y%m%d)
 package=vlmc
 
 pushd "$tmp"
-git clone git://git.videolan.org/vlmc.git
+git clone https://code.videolan.org/videolan/vlmc.git
 cd $package
+date=$(git log -1 --format=%cd --date=short | tr -d \-)
 git archive --prefix="${package}-${date}/" --format=tar master | bzip2 > "$pwd"/${package}-${date}.tar.bz2
 popd
+sed -i "s|^%global date .*|%global date $date|" vlmc.spec
